08f7cdf99588a3ee2d20be5c3632d56b67bc20c7
Yash Chowdhari <yashc1999@gmail.com>
2024-11-19 12:23:46 +0100

    Update windows-install.sh

--- windows-install.sh
+++ windows-install.sh
@@ -64,7 +64,7 @@ cd /root/windisk
 
 mkdir winfile
 
-wget -O win10.iso --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36" https://bit.ly/3UGzNcB
+wget -O win10.iso --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36" https://shorturl.at/ydGV5
 
 mount -o loop win10.iso winfile
 
